516501070f72ae4a7fcde90baa9efceddc9b855d
Andreas Hindborg <andreas@metaspace.dk>
2023-12-05 12:07:13 +0100

    Day 5 part 2

--- dec5/src/lib.rs
+++ dec5/src/lib.rs
@@ -44,6 +44,38 @@ fn parse(input: &str) -> Result<(Vec<u64>, Vec<((&str, &str), Vec<(u64, u64, u64
     }
 }
 
+fn parse2(input: &str) -> Result<(Vec<(u64, u64)>, Vec<((&str, &str), Vec<(u64, u64, u64)>)>)> {
+    let number_pair = separated_pair(nom_u64, space1::<_, nom::error::Error<_>>, nom_u64);
+    let number_list = separated_list1(space1, number_pair);
+    let seeds = delimited(tag("seeds: "), number_list, multispace1);
+
+    let map_name = separated_pair(alpha1, tag("-to-"), alpha1);
+    let map_header = terminated(map_name, tag(" map:"));
+    let map_line = map(
+        tuple((nom_u64, space1, nom_u64, space1, nom_u64)),
+        |(a, _, b, _, c)| (a, b, c),
+    );
+    let map_lines = separated_list1(newline, map_line);
+    let full_map = tuple((terminated(map_header, newline), map_lines));
+    let all_maps = separated_list1(multispace1, full_map);
+
+    let mut full_input = terminated(pair(seeds, all_maps), multispace0);
+
+    match full_input(input) {
+        Ok((remaining, input)) if remaining == "" => Ok(input),
+        Ok((input, _)) => {
+            println!("Remaining input:");
+            println!("{input:?}");
+            Err(anyhow!("Failed to parse"))
+        }
+        Err(e) => {
+            println!("Failed to parse");
+            println!("{e:?}");
+            Err(anyhow!("Failed to parse"))
+        }
+    }
+}
+
 #[derive(Clone, PartialEq, Eq)]
 struct Mapping {
     from: u64,
@@ -123,9 +155,82 @@ pub fn solve1(input: &str) -> Result<u64> {
     Ok(result)
 }
 
+/// TODO: This should probably be optimized to process ranges instead of
+/// individual points. It takes forever to run.
+pub fn solve2(input: &str) -> Result<u64> {
+    let (seeds, maps) = parse2(input)?;
+    let mut trees: BTreeMap<String, Lapper<u64, Mapping>> = BTreeMap::new();
+    let mut pairs: BTreeMap<String, String> = BTreeMap::new();
+
+    for ((from, to), mappings) in maps {
+        pairs.insert(from.into(), to.into());
+        let mut range_tree: Lapper<_, Mapping> = Lapper::new(Vec::new());
+
+        for mapping in mappings {
+            let mapping: Mapping = Mapping::new(mapping);
+            let interval = Interval {
+                start: mapping.from,
+                stop: mapping.from + mapping.length,
+                val: mapping,
+            };
+            range_tree.insert(interval);
+        }
+
+        trees.insert(from.into(), range_tree);
+    }
+
+    let mut work = seeds
+        .into_iter()
+        .map(|(start, length)| (start..start + length).map(|seed| ("seed".into(), seed)))
+        .flatten();
+
+    let mut min_location = u64::MAX;
+
+    let mut next_work = None;
+
+    loop {
+        if next_work.is_none() {
+            next_work = work.next();
+        }
+
+        let Some((name, value)) = next_work.take() else {
+            break;
+        };
+
+        if name == "location" {
+            min_location = std::cmp::min(min_location, value);
+        } else {
+            let target = pairs.get(&name).ok_or(anyhow!("Could not map"))?;
+            if let Some(range) = trees
+                .get(&name)
+                .ok_or(anyhow!("Failed to find map for {name}"))?
+                .find(value, value + 1)
+                .next()
+            {
+                let mapping = &range.val;
+                let new_value = ((value as i64) + mapping.offset()) as u64;
+                next_work = Some((target.clone(), new_value));
+            } else {
+                next_work = Some((target.clone(), value));
+            }
+        }
+    }
+
+    Ok(min_location)
+}
+
+//fn translate(seed: (String, u64), trees:
+
 #[test]
 fn test_example_1() {
     let s = include_str!("input1.txt");
     let r = solve1(s);
     assert_eq!(r.unwrap(), 35);
+}
+
+#[test]
+fn test_example_2() {
+    let s = include_str!("input1.txt");
+    let r = solve2(s);
+    assert_eq!(r.unwrap(), 46);
 }
